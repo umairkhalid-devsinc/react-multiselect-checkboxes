0707bdbda649022fb742d9d91acb646c667f893b
Umair Khalid <umair.khalid@devsinc.com>
2023-08-25 16:22:03 +0500

    replaced emotion with emotion/css

--- lib/CheckboxGroupHeading.jsx
+++ lib/CheckboxGroupHeading.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { css } from "emotion";
+import { css } from '@emotion/css';
 
 class CheckboxWithIndeterminate extends React.Component {
   static propTypes = {
