d4ce0a21e2e96b43c34816397d1bd961364837b4
Umair Khalid <umair.khalid@devsinc.com>
2023-08-28 15:11:47 +0500

    move all compiled files to src folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './lib/index.js', // The main entry point of your application
+  entry: './src/lib/index.js', // The main entry point of your application
   output: {
     path: path.resolve(__dirname, 'dist'), // Output directory
     filename: 'bundle.js', // Output bundle file name
@@ -15,10 +15,10 @@ module.exports = {
           loader: 'babel-loader', // Use Babel for transpiling ES6+ code
         },
       },
-    //   {
-    //     test: /\.css$/, // Apply the following loaders to .css files
-    //     use: ['style-loader', 'css-loader'], // Use style-loader and css-loader
-    //   },
+      {
+        test: /\.css$/, // Apply the following loaders to .css files
+        use: ['style-loader', 'css-loader'], // Use style-loader and css-loader
+      },
     ],
   },
 };
